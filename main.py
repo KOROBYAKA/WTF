14c8a8fe352c5229509eb629fd096c153537d412
KOROBYAKA <109600101+KOROBYAKA@users.noreply.github.com>
2022-07-19 16:10:47 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+import os
+import subprocess
+import sys
+
+def main():
+    run_cfg = subprocess.run(["iperf3.exe","-c speedtest.serverius.net -p5202"])
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
