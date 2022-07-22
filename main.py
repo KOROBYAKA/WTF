f4876f65b37b5f0fa707e900b363f083ca88554b
KOROBYAKA <109600101+KOROBYAKA@users.noreply.github.com>
2022-07-22 13:03:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-
-
-
 import subprocess
-
-
+import sys
+from sys import argv
 def main():
-    cfg = ("iperf3.exe")
-    act = subprocess.run(cfg, input("iperf3 -c speedtest.hostkey.ru -p5202"))
+    host, port = argv
+    exe = ("iperf3.exe iperf3" + host + port+ "--logfile iperf.log")###-c speedtest.serverius.net -p5002
+    act = subprocess.run(exe)
 
-
-
+    subprocess.run("notepad iperf.log")
 if __name__ == '__main__':
     main()
