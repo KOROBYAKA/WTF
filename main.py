3459117a76f4f4183b7263681a3c4572ea2dfcad
KOROBYAKA <109600101+KOROBYAKA@users.noreply.github.com>
2022-07-22 13:27:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import subprocess
 import sys
 from sys import argv
 def main():
-    host, port = argv
-    exe = ("iperf3.exe iperf3" + host + port+ "--logfile iperf.log")###-c speedtest.serverius.net -p5002
+    status,host, port = argv
+    exe = ("iperf3.exe iperf3"+ status + host + port+ "--logfile iperf.log")###-c speedtest.serverius.net -p5002
     act = subprocess.run(exe)
 
     subprocess.run("notepad iperf.log")
