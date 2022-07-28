75e5015c67843b82c5db57f0ef806282ac0268ee
KOROBYAKA <109600101+KOROBYAKA@users.noreply.github.com>
2022-07-28 16:17:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,44 @@
 import subprocess
 import sys
 from sys import argv
+import platform
 import os.path
+import argparse
 def main():
-    try:
-        script, host, port,bw = argv
-        port = "-p"+port
-        data = [host,port]
-        xx = " ".join(data)
+    sys = platform.system()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-host','--host',help = '''Host's IP address''')
+    parser.add_argument('-BW','-bw','--bandwidth', default = 0, help = 'Bandwidth optional argument(default 0)')
+    parser.add_argument('-p','--port',default = 5201, help = 'Port for connection to server')
+    args = parser.parse_args()
+    port = args.port
+    host = args.host
+    bw = args.bandwidth
 
-    except ValueError:
-        pass
-        print('''Give arguments or use manual config or test for test''')
-        xx = input('''For example "*host IP* -p*port number* *bandwidth*" \n -->>''')
+
 
     x = 1
     xstr = str(x)
-    list = ["iperf_test_N.", xstr, ".log"]
+    fn = ["iperf_test_N.", xstr, ".log"]
     chk = 1
 
     while chk == 1 :
 
-        file = "".join(list)
+        file = "".join(fn)
         file_check = os.path.isfile(file)
         if file_check == True:
             x += 1
             xstr = str(x)
-            list = ["iperf_test_N.", xstr, ".log"]
+            fn = ["iperf_test_N.", xstr, ".log"]
         elif file_check == False:
             chk = 0
     crt = open(file,"w")
+    crt.close()
 
-    if xx != "manual" and xx != "test":
-        xx = xx.split(" ")
-        xx[-1] = "--bandwidth" + xx[-1]
-        xx = "".join(xx)
-        cfg = ("iperf3.exe iperf3 -c " + xx + " --length 6472 -u --bytes 1342177280  --logfile " + file)
-    if xx == "manual":
-        xx = input("Give Iperf3 startup config: ")
-        cfg = ("iperf3.exe iperf3 " + xx)
-    if xx == "test":
-        cfg = ("iperf3.exe iperf3 -c iperf.eenet.ee -p5201 --logfile iperf_test.log")
-        file = "iperf_test.log"
-
+    if sys == 'Windows':
+        cfg = (f"iperf3.exe  iperf3 -c {host} -p{port} {bw} --length 6472 -u --bytes 1342177280 -logfile {file}")
+    elif sys = 'Linux':
+        
 
     act = subprocess.run(cfg)
 
